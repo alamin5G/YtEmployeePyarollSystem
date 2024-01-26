4c6b030d1ba81df80b87614449e84884d1ad69a9
alamin5g <21303134@iubat.edu>
2024-01-27 04:14:54 +0600

    Payroll Management System

--- src/com/goonok/abstracttion/Employee.java
+++ src/com/goonok/abstracttion/Employee.java
@@ -1,2 +1,14 @@
-package com.goonok.abstracttion;public class Employee {
+package com.goonok.abstracttion;
+
+public abstract class Employee {
+    private String name;
+    private int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    ///TODO - 8:30 MINUTES https://www.youtube.com/watch?v=o-JnANhtMVM
+
 }
